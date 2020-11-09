ce90ed75adb348829110ab3afa01aa96449bdb57
Cano <contact.sasasaky@gmail.com>
2020-11-09 20:06:09 +0900

    upload multiple files with glob pattern

--- src/upload-release-asset.js
+++ src/upload-release-asset.js
@@ -1,5 +1,7 @@
 const core = require('@actions/core');
 const { GitHub } = require('@actions/github');
+const glob = require('@actions/glob');
+const mime = require('mime-types');
 const fs = require('fs');
 
 async function run() {
@@ -11,31 +13,35 @@ async function run() {
     const uploadUrl = core.getInput('upload_url', { required: true });
     const assetPath = core.getInput('asset_path', { required: true });
     const assetName = core.getInput('asset_name', { required: true });
-    const assetContentType = core.getInput('asset_content_type', { required: true });
+    // const assetContentType = core.getInput('asset_content_type', { required: true });
 
     // Determine content-length for header to upload asset
     const contentLength = filePath => fs.statSync(filePath).size;
 
-    // Setup headers for API call, see Octokit Documentation: https://octokit.github.io/rest.js/#octokit-routes-repos-upload-release-asset for more information
-    const headers = { 'content-type': assetContentType, 'content-length': contentLength(assetPath) };
+    const globber = await glob.create(assetPath);
 
     // Upload a release asset
     // API Documentation: https://developer.github.com/v3/repos/releases/#upload-a-release-asset
     // Octokit Documentation: https://octokit.github.io/rest.js/#octokit-routes-repos-upload-release-asset
-    const uploadAssetResponse = await github.repos.uploadReleaseAsset({
-      url: uploadUrl,
-      headers,
-      name: assetName,
-      file: fs.readFileSync(assetPath)
-    });
+    for await (const filePath of globber.globGenerator()) {
+      // Setup headers for API call, see Octokit Documentation: https://octokit.github.io/rest.js/#octokit-routes-repos-upload-release-asset for more information
+      const headers = { 'content-type': mime.lookup(file), 'content-length': contentLength(filePath) };
+
+      await github.repos.uploadReleaseAsset({
+        url: uploadUrl,
+        headers,
+        name: assetName,
+        file: fs.readFileSync(filePath)
+      });
+    }
 
     // Get the browser_download_url for the uploaded release asset from the response
-    const {
-      data: { browser_download_url: browserDownloadUrl }
-    } = uploadAssetResponse;
+    // const {
+    //   data: { browser_download_url: browserDownloadUrl }
+    // } = uploadAssetResponse;
 
     // Set the output variable for use by other actions: https://github.com/actions/toolkit/tree/master/packages/core#inputsoutputs
-    core.setOutput('browser_download_url', browserDownloadUrl);
+    // core.setOutput('browser_download_url', browserDownloadUrl);
   } catch (error) {
     core.setFailed(error.message);
   }
